bcd579a014ca72ec6493482878a6869123112770
GabrielAmare <67603418+GabrielAmare@users.noreply.github.com>
2021-07-06 02:52:10 +0200

    Update BaseFile.py
    added exists method that check if a file exists

--- tools37/files/BaseFile.py
+++ tools37/files/BaseFile.py
@@ -5,6 +5,10 @@ class BaseFile:
     extension: str = ""
 
     @classmethod
+    def exists(cls, fp: str) -> bool:
+        return os.path.exists(cls._parse_fp(fp))
+
+    @classmethod
     def _parse_fp(cls, fp: str) -> str:
         if fp.endswith(cls.extension):
             return fp
