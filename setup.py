950925c37774bfbbd785fe66bfb830504b3ffd55
GabrielAmare <67603418+GabrielAmare@users.noreply.github.com>
2021-08-03 00:44:26 +0200

    Update setup.py
    packages are now on their own line to simplify review when editing the package list

--- setup.py
+++ setup.py
@@ -9,8 +9,15 @@ if __name__ == "__main__":
         author_email="gabriel.amare.dev@gmail.com",
         description="various tools & utilitaries for python 3.7",
         url="https://github.com/GabrielAmare/Tools37",
-        packages=["tools37", "tools37.files", "tools37.actions", "tools37.console", "tools37.logger", "tools37.events",
-                  "tools37.geom"],
+        packages=[
+            "tools37",
+            "tools37.files",
+            "tools37.actions",
+            "tools37.console",
+            "tools37.logger",
+            "tools37.events",
+            "tools37.geom"
+        ],
         classifiers=[],
         python_requires=">=3.7"
     )
