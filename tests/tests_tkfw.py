2e3485096606517b27648b881166b9526a140d32
GabrielAmare <67603418+GabrielAmare@users.noreply.github.com>
2021-11-21 04:23:32 +0100

    Update tests_tkfw.py
    created TestEvaluableListItem

--- tests/tests_tkfw.py
+++ tests/tests_tkfw.py
@@ -1,6 +1,6 @@
 import unittest
 
-from tools37.tkfw.evaluable import EvaluableDictItem
+from tools37.tkfw.evaluable import EvaluableDictItem, EvaluableListItem
 
 
 class TestEvaluableDictItem(unittest.TestCase):
@@ -34,6 +34,37 @@ class TestEvaluableDictItem(unittest.TestCase):
         data = {"key": "value"}
         EvaluableDictItem("key").set(data, "modified")
         self.assertEqual(data["key"], "modified")
+
+
+class TestEvaluableListItem(unittest.TestCase):
+    def test_method_evaluate(self):
+        # on missing index
+        self.assertRaises(IndexError, EvaluableListItem(index=0).evaluate, [])
+
+        # on wrong data type
+        self.assertRaises(TypeError, EvaluableListItem(index=0).evaluate, {})
+
+        # standard behaviour
+        self.assertEqual("value", EvaluableListItem(index=0).evaluate(["value"]))
+
+    def test_method_get(self):
+        # on missing index
+        self.assertRaises(IndexError, EvaluableListItem(index=0).get, [])
+
+        # on wrong data type
+        self.assertRaises(TypeError, EvaluableListItem(index=0).get, {})
+
+        # standard behaviour
+        self.assertEqual("value", EvaluableListItem(index=0).get(["value"]))
+
+    def test_method_set(self):
+        # on missing index
+        self.assertRaises(IndexError, EvaluableListItem(index=0).set, data=[], value="value")
+
+        # standard behaviour
+        data = ["value"]
+        EvaluableListItem(index=0).set(data, "modified")
+        self.assertEqual(data[0], "modified")
 
 
 if __name__ == '__main__':
