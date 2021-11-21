d166fdc10bb26936d64d91dc7ab8b5e076226aac
GabrielAmare <67603418+GabrielAmare@users.noreply.github.com>
2021-11-21 04:53:12 +0100

    created TestEvaluablePath

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,7 +5,7 @@ from tests.test_geom import TestVector, TestCoords, TestVectorBase
 from tests.test_physics import TestPhysic
 from tests.test_formats import TestFormats
 from tests.test_events import TestEvents
-from tests.tests_tkfw import TestEvaluable
+from tests.tests_tkfw import TestEvaluableDictItem, TestEvaluableListItem, TestEvaluablePath
 
 if __name__ == '__main__':
     unittest.main()
--- tests/tests_tkfw.py
+++ tests/tests_tkfw.py
@@ -1,6 +1,6 @@
 import unittest
 
-from tools37.tkfw.evaluable import EvaluableDictItem, EvaluableListItem
+from tools37.tkfw.evaluable import EvaluableDictItem, EvaluableListItem, EvaluablePath
 
 
 class TestEvaluableDictItem(unittest.TestCase):
@@ -67,5 +67,82 @@ class TestEvaluableListItem(unittest.TestCase):
         self.assertEqual(data[0], "modified")
 
 
-if __name__ == '__main__':
-    unittest.main()
+class TestEvaluablePath(unittest.TestCase):
+    def test_method_evaluate(self):
+        # on wrong type 1
+        path = EvaluablePath(steps=[EvaluableDictItem("key"), EvaluableListItem(0)])
+        self.assertRaises(TypeError, path.evaluate, data=[])
+
+        # on missing key 1
+        path = EvaluablePath(steps=[EvaluableDictItem("key"), EvaluableListItem(0)])
+        self.assertRaises(KeyError, path.evaluate, data={})
+
+        # on wrong type 2
+        path = EvaluablePath(steps=[EvaluableDictItem("key"), EvaluableListItem(0)])
+        self.assertRaises(TypeError, path.evaluate, data={"key": {}})
+
+        # on missing index 2
+        path = EvaluablePath(steps=[EvaluableDictItem("key"), EvaluableListItem(0)])
+        self.assertRaises(IndexError, path.evaluate, data={"key": []})
+
+        # standard behaviour
+        path = EvaluablePath(steps=[EvaluableDictItem("key"), EvaluableListItem(0)])
+        self.assertEqual("value", path.evaluate(data={"key": ["value"]}))
+
+    def test_method_get(self):
+        # on wrong type 1
+        path = EvaluablePath(steps=[EvaluableDictItem("key"), EvaluableListItem(0)])
+        self.assertRaises(TypeError, path.get, data=[])
+
+        # on missing key 1
+        path = EvaluablePath(steps=[EvaluableDictItem("key"), EvaluableListItem(0)])
+        self.assertRaises(KeyError, path.get, data={})
+
+        # on wrong type 2
+        path = EvaluablePath(steps=[EvaluableDictItem("key"), EvaluableListItem(0)])
+        self.assertRaises(TypeError, path.get, data={"key": {}})
+
+        # on missing index 2
+        path = EvaluablePath(steps=[EvaluableDictItem("key"), EvaluableListItem(0)])
+        self.assertRaises(IndexError, path.get, data={"key": []})
+
+        # standard behaviour
+        path = EvaluablePath(steps=[EvaluableDictItem("key"), EvaluableListItem(0)])
+        self.assertEqual("value", path.get(data={"key": ["value"]}))
+
+    def test_method_set(self):
+        # on wrong type 1
+        path = EvaluablePath(steps=[EvaluableDictItem("key"), EvaluableListItem(0)])
+        self.assertRaises(TypeError, path.set, data=[], value=None)
+
+        # on missing key 1
+        path = EvaluablePath(steps=[EvaluableDictItem("key"), EvaluableListItem(0)])
+        self.assertRaises(KeyError, path.set, data={}, value=None)
+
+        # on wrong type 2
+        path = EvaluablePath(steps=[EvaluableDictItem("key"), EvaluableListItem(0)])
+        self.assertRaises(TypeError, path.set, data={"key": {}}, value=None)
+
+        # on missing index 2
+        path = EvaluablePath(steps=[EvaluableDictItem("key"), EvaluableListItem(0)])
+        self.assertRaises(IndexError, path.set, data={"key": []}, value=None)
+
+        # standard behaviour
+        path = EvaluablePath(steps=[EvaluableDictItem("key"), EvaluableListItem(0)])
+        data = {"key": ["value"]}
+        path.set(data=data, value="modified")
+        self.assertEqual("modified", data["key"][0])
+
+    def test_dunder_str(self):
+        # standard behaviour
+        self.assertEqual("key.0", str(EvaluablePath(steps=[EvaluableDictItem("key"), EvaluableListItem(0)])))
+
+    def test_dunder_add(self):
+        left = EvaluablePath(steps=[EvaluableDictItem("key")])
+        right = EvaluablePath(steps=[EvaluableListItem(0)])
+
+        # standard behaviour
+        self.assertEqual(EvaluablePath(steps=left.steps + right.steps), left + right)
+
+    if __name__ == '__main__':
+        unittest.main()
