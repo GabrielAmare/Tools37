0fbf0f17075943adc522dd0a98ea65b212209bbf
GabrielAmare <67603418+GabrielAmare@users.noreply.github.com>
2021-11-21 04:00:48 +0100

    created TestEvaluable for tkfw tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,6 +5,7 @@ from tests.test_geom import TestVector, TestCoords, TestVectorBase
 from tests.test_physics import TestPhysic
 from tests.test_formats import TestFormats
 from tests.test_events import TestEvents
+from tests.tests_tkfw import TestEvaluable
 
 if __name__ == '__main__':
     unittest.main()
--- tests/tests_tkfw.py
+++ tests/tests_tkfw.py
@@ -1,4 +1,9 @@
 import unittest
 
+
+class TestEvaluable(unittest.TestCase):
+    pass
+
+
 if __name__ == '__main__':
     unittest.main()
